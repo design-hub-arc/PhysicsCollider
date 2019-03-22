808582e82c3d9c346f8d81ad01128334623029b4
Keddari <W1600786@ARC.losrios.edu>
2019-03-22 12:59:06 -0700

    Added functionality to main menu buttons

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    //we want to store all the menu data in this class
+
+    public static class MenuInput
+    {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+    
+    
+   /* // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+*/
