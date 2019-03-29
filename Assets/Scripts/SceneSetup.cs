5a8766cd5e6fc0a7f88fba8d34c771dffb59ca6d
Blanco <W1651759@ARC.losrios.edu>
2019-03-29 13:15:38 -0700

    initial particle setup

---
+++ Assets/Scripts/SceneSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSetup : MonoBehaviour {
+
+    public Input UIInput;
+    public GameObject Particle1;
+    public GameObject Particle2;
+
+    void StartFromUI()
+    {
+        Input.ParticleSetup particleSetup = UIInput.GetUserInput();
+    }
+
+    void StartFromFile()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
