dce1509a43b918a9d4c3973aa38bc7af7c01433a
Keddari <W1600786@ARC.losrios.edu>
2019-04-05 13:19:30 -0700

    completed patrticle setup

--- Assets/Scripts/SceneSetup.cs
+++ Assets/Scripts/SceneSetup.cs
@@ -4,22 +4,67 @@ using UnityEngine;
 
 public class SceneSetup : MonoBehaviour {
 
+    public GameObject menu;
+    public GameObject simUI;
+
     public Input UIInput;
     public GameObject Particle1;
     public GameObject Particle2;
 
-    void StartFromUI()
+    Input.ParticleSetup prevParticleSetup;
+
+    public void StartFromUI()
     {
         Input.ParticleSetup particleSetup = UIInput.GetUserInput();
+        Setup(particleSetup);
+
     }
 
-    void StartFromFile()
+    public void StartFromFile()
     {
 
     }
+    void Setup(Input.ParticleSetup particleSetup)
+    {
+        Transform transformP1 = Particle1.transform;
+        Rigidbody rigidbodyP1 = Particle1.GetComponent<Rigidbody>();
 
-	// Use this for initialization
-	void Start () {
+        Transform transformP2 = Particle2.transform;
+        Rigidbody rigidbodyP2 = Particle2.GetComponent<Rigidbody>();
+
+
+        transformP1.localScale = Vector3.one * particleSetup.particleArray[0].size;
+        transformP1.localPosition = particleSetup.particleArray[0].position;
+        rigidbodyP1.velocity = particleSetup.particleArray[0].velocity;
+
+        transformP2.localScale = Vector3.one * particleSetup.particleArray[1].size;
+        transformP2.localPosition = particleSetup.particleArray[1].position;
+        rigidbodyP2.velocity = particleSetup.particleArray[1].velocity;
+
+
+        prevParticleSetup = particleSetup;
+        ShowMenu(false);
+    }
+
+    public void Reset()
+    {
+        Setup(prevParticleSetup);
+    }
+
+    public void ShowSimUI(bool enable)
+    {
+        simUI.SetActive(enable);
+        ShowMenu(!enable);
+    }
+
+    public void ShowMenu(bool enable)
+    {
+        menu.SetActive(enable);
+        ShowSimUI(!enable);
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 }
