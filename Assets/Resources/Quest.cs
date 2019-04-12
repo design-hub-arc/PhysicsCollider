dd79c68d51fa8aa39091a4073c816fbb768efb04
Keddari <W1600786@ARC.losrios.edu>
2019-04-12 14:55:29 -0700

    added CSV file functionality

---
+++ Assets/Resources/Quest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+    //all variables in here
+
+    public double X1;
+    public double Y1;
+    public double Z1;
+    public double X2;
+    public double Y2;
+    public double Z2;
+
+
+}
