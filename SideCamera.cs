433b125faa8e825967eaf3ad4a1cea0cd10d10dd
KouVueARC <48299802+KouVueARC@users.noreply.github.com>
2019-05-01 16:15:04 -0700

    Add files via upload

---
+++ SideCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideCamera : MonoBehaviour {
+
+    //Rigidbody rb;
+
+    public GameObject targetToFollow;
+
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = targetToFollow.transform.position + offset;
+
+    }
+}
